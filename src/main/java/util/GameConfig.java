441922668d57e731a547151f2bb5ecde54a01177
tharawaranuset <tharawaranuset@gmail.com>
2026-04-26 19:02:54 +0700

    refactor: constant variables

---
+++ src/main/java/util/GameConfig.java
@@ -0,0 +1,11 @@
+package util;
+
+public class GameConfig {
+
+    public static final int TILE_SIZE = 48;
+    public static final int COLS = 16;
+    public static final int ROWS = 10;
+
+    private GameConfig() {
+    }  // prevent new GameConfig()
+}
